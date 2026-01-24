5562c68908f57893b58beb80bc8a38777c3d2daa
Sarthak Katta <sarthakkatta7@gmail.com>
2026-01-24 13:48:27 +0530

    Create make_network_connected_dsu.cpp

---
+++ Graphs/make_network_connected_dsu.cpp
@@ -0,0 +1,85 @@
+/*
+ Problem: Number of Operations to Make Network Connected
+
+ Approach:
+ - Use Disjoint Set Union (Union-Find)
+ - Count extra edges (edges forming cycles)
+ - Count number of connected components
+ - To connect C components, we need (C - 1) edges
+ - If extra edges >= (C - 1), return (C - 1), else return -1
+
+ Time Complexity: O(N + E * α(N))
+ Space Complexity: O(N)
+*/
+
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int cntExtras = 0;
+        for (auto it : connections) {
+            int u = it[0];
+            int v = it[1];
+            if (ds.findUPar(u) == ds.findUPar(v)) {
+                cntExtras++;
+            }
+            else {
+                ds.unionBySize(u, v);
+            }
+        }
+        int cntC = 0;
+        for (int i = 0; i < n; i++) {
+            if (ds.parent[i] == i) cntC++;
+        }
+        int ans = cntC - 1;
+        if (cntExtras >= ans) return ans;
+        return -1;
+    }
+};
